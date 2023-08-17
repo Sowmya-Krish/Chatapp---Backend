3012a64cf4f110e48a14833b8852fea7bf250fce
Sowmya-Krish <sowmikrish.crazy@gmail.com>
2023-08-17 16:00:53 +0530

    chatapp

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const url = `mongodb+srv://chatapp:chatapp@cluster0.pae0gpr.mongodb.net/?retryWrites=true&w=majority`;
+const url = `mongodb+srv://chatapp:chatapp@cluster0.pae0gpr.mongodb.net/Chatapp?retryWrites=true&w=majority`;
 
-mongoose.connect(url, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-}).then(() => console.log('Connected to DB')).catch((e)=> console.log('Error', e))
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to DB"))
+  .catch((e) => console.log("Error", e));
